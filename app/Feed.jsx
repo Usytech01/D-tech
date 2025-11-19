98cb71956a098f586ccae296c950d5d1f56129a0
Usytech01 <usmandioo@yahoo.com>
2025-11-19 10:21:20 +0100

    Adjust Feed width

--- app/Feed.jsx
+++ app/Feed.jsx
@@ -5,7 +5,7 @@ import Post from "./component/Post"
 
 export default function Feed() {
   return (
-    <div className='md:[75vw] lg:w-]35vw] px-5 py-6'>
+    <div className='md:[75vw] lg:w-[45vw] px-5 py-6'>
       <Share/>
       {Posts.map(p => (
         <Post key={p.id} post={p}/>
